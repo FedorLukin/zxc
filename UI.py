0d0cfd22f29dc90b585e677d1f380942b9eed598
Фёдор Лукин <fedor47365@yandex.ru>
2024-11-30 15:15:41 +0700

    UI.ui file replaced with UI.py

---
+++ UI.py
@@ -0,0 +1,18 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setGeometry(QtCore.QRect(140, 80, 111, 111))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Press me!"))
